3b076505018b25ac9cf713af1b1447129dfe17ba
Midas Yoon <goback2u@naver.com>
2022-01-23 19:24:14 +0900

    PageRequest

---
+++ src/main/java/com/midas/yoon/config/web/SimplePageRequest.java
@@ -0,0 +1,30 @@
+package com.midas.yoon.config.web;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+public class SimplePageRequest {
+    private final int page;
+    private final int size;
+
+    public SimplePageRequest(int page, int size) {
+        if (page < 1 || page > Integer.MAX_VALUE) page = 1;
+        if (size < 1 || size > 10) size = 10;
+
+        this.page = page;
+        this.size = size;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Pageable makePageable(String sortColumn) {
+        return PageRequest.of(page - 1, size, Sort.by(sortColumn).descending());
+    }
+}
